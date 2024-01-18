c9f88e0ded7a49b3467f1f09058fb22cda3abc02
YONGWON SEO.DEV <bincentiver@gmail.com>
2024-01-19 00:40:15 +0900

    Update crossjoin_example.sql

--- SQL/crossjoin_example.sql
+++ SQL/crossjoin_example.sql
@@ -5,4 +5,26 @@ WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.tempera
 /*
 Using duplicate of tables and join cross. Datediff used for difference between two dates. If you don't use join and instead 
 math normally such as 2011-12-31 + 1, sql calculated 2011-12-32, which could lead to wrong values.
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
 */
