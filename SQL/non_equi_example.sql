85cbba3811970c1b93b83ee2fcc87ab6ac33c712
YONGWON SEO.DEV <bincentiver@gmail.com>
2024-01-19 00:38:58 +0900

    Create non_equi_example.sql

---
+++ SQL/non_equi_example.sql
@@ -0,0 +1,42 @@
+
+select p.product_id, coalesce(round(SUM(U.units*P.price)/SUM(u.units),2),0) as average_price
+from Prices p
+    left join UnitsSold u on p.product_id = u.product_id
+    AND u.purchase_date between start_date and end_date
+group by 1
+/*
+usual equijoin like this: e.date = p.date. But when want to join the value in interval, especially for dates, you can use 
+non-equijoin by using between start_date and end_date such in this problem.
+Look at we don't use avg function since we have to use u.units*p.price and also have to use left join function, which there will
+be null value.
+
+Prices table:
++------------+------------+------------+--------+
+| product_id | start_date | end_date   | price  |
++------------+------------+------------+--------+
+| 1          | 2019-02-17 | 2019-02-28 | 5      |
+| 1          | 2019-03-01 | 2019-03-22 | 20     |
+| 2          | 2019-02-01 | 2019-02-20 | 15     |
+| 2          | 2019-02-21 | 2019-03-31 | 30     |
++------------+------------+------------+--------+
+UnitsSold table:
++------------+---------------+-------+
+| product_id | purchase_date | units |
++------------+---------------+-------+
+| 1          | 2019-02-25    | 100   |
+| 1          | 2019-03-01    | 15    |
+| 2          | 2019-02-10    | 200   |
+| 2          | 2019-03-22    | 30    |
++------------+---------------+-------+
+Output: 
++------------+---------------+
+| product_id | average_price |
++------------+---------------+
+| 1          | 6.96          |
+| 2          | 16.96         |
++------------+---------------+
+Explanation: 
+Average selling price = Total Price of Product / Number of products sold.
+Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
+Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
+*/
