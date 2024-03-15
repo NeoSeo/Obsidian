c32b9458b842d5388e8ea3d43ab52e908607e174
YONGWON SEO.DEV <bincentiver@gmail.com>
2024-03-16 04:20:21 +0900

    Create Employee_without_manager.sql

---
+++ SQL/leetcode/Employee_without_manager.sql
@@ -0,0 +1,22 @@
+/*
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| manager_id  | int      |
+| salary      | int      |
++-------------+----------+
+In SQL, employee_id is the primary key for this table.
+This table contains information about the employees, their salary, and the ID of their manager. Some employees do not have a manager (manager_id is null). 
+Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
+Return the result table ordered by employee_id.
+The result format is in the following example.
+*/
+
+
+select employee_id
+from Employees
+where salary < 30000 and manager_id IS NOT NULL and manager_id not in (select employee_id from Employees)
+order by employee_id
+
