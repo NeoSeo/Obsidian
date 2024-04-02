cf818a7fd546e85ac3a78a6d4a523300e7d57a07
YONGWON SEO.DEV <bincentiver@gmail.com>
2024-04-03 04:06:54 +0900

    Create limit_offset.sql

---
+++ SQL/leetcode/limit_offset.sql
@@ -0,0 +1,68 @@
+-- 1st solution
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1)  -- using LIMIT/OFFSET to find 2nd or later values
+as SecondHighestSalary;
+
+-- 2nd solution (wrong)
+with cte as (
+select id, salary, dense_rank() over (order by salary DESC) as salrank
+from Employee
+)
+select id, salary -- NULL doesn't show up
+from cte
+where salrank = 2
+
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
